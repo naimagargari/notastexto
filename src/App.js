205b8cba8dd42b393a219a478ae5fe6fdbe732da
Naima El Gargari Sampedro <145050656+naimagargari@users.noreply.github.com>
2024-01-31 17:51:09 +0100

    pequeños cambios

--- src/App.js
+++ src/App.js
@@ -1,53 +1,53 @@
-// import React from "react";
-
-// function App() {
-//   return (
-//     <div>
-//       <h1>Bienvenido a mi aplicación de notas de texto</h1>
-//       <p>Esta es la página principal de la aplicación.</p>
-//       {/* Puedes agregar más componentes y contenido aquí */}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// src/App.js
 import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
   const [notas, setNotas] = useState([]);
 
   useEffect(() => {
-    // Realizar una solicitud GET al endpoint /api/notas del backend
-    fetch("http://localhost:3000/api/notas")
-      .then((response) => response.json())
-      .then((data) => {
-        // Console.log para verificar el contenido de las notas
+    const fetchNotas = async () => {
+      try {
+        // Realizar una solicitud GET al endpoint 
+        const response = await fetch("http://localhost:3000/api/notas");
+
+        if (!response.ok) {
+          throw new Error("Error al obtener notas desde el backend");
+        }
+
+        // Verificar el contenido de las notas
+        const data = await response.json();
         console.log("Notas recibidas desde el backend:", data);
 
         setNotas(data);
-      })
-      .catch((error) => {
-        // console.error("Error al obtener notas desde el backend:", error);
-      });
+      } catch (error) {
+        console.error(error.message);
+        // Manejar el error de manera adecuada(mostrar un mensaje al usuario)
+      }
+    };
+
+    // Llamar a la función de obtención de notas
+    fetchNotas();
   }, []); 
 
   return (
     <div>
-      <h1>Bienvenido a mi aplicación de notas de texto</h1>
-      <p>Esta es la página principal de la aplicación.</p>
+      <header>
+        <h1>Bienvenidos a mi aplicación de notas de texto</h1>
+        <p>Esta es la página principal de la aplicación.</p>
+      </header>
 
-      <h2>Notas:</h2>
-      <ul>
-        {notas.map((nota) => (
-          <li key={nota.id}>
-            <strong>{nota.titulo}:</strong> {nota.texto}
-          </li>
-        ))}
-      </ul>
+      <section>
+        <h2>Notas:</h2>
+        <ul>
+          {notas.map((nota) => (
+            <li key={nota.id}>
+              <strong>{nota.titulo}:</strong> {nota.texto}
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
+
